411c3c28ea45252fa39cb50f1d1d5775f5007b09
Sergey An <ansergeyg@gmail.com>
2020-04-02 12:13:48 +0000

    DAE-3551: Commented the settings.override.php file.

---
+++ settings.override.php
@@ -0,0 +1,10 @@
+<?php
+$databases["sparql_default"] = array(
+  'default' => array(
+    'prefix' => '', 
+    'host' => 'sparql',
+    'port' => '8890',
+    'namespace' => 'Drupal\\Driver\\Database\\sparql',
+    'driver' => 'sparql'
+  )
+);
